8db2f3018e0f84363dcbfa55c258c5a2f798bcfe
Jason <jason.lo.kc@gmail.com>
2020-11-29 23:33:25 -0800

    commit

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -1,5 +1,32 @@
 import React from "react";
-import Hero1 from "../assets/home/mobile/";
+//set the pictures as background images so they can use different images at different dimension
+
+let hero = [
+  {
+    id: 1,
+    title: "Project Paramour",
+    description:
+      "Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
+  },
+  {
+    id: 2,
+    title: "Seraph Station",
+    description:
+      "The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
+  },
+  {
+    id: 3,
+    title: "Federal II Tower",
+    description:
+      "A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
+  },
+  {
+    id: 4,
+    title: "Trinity Bank Tower",
+    description:
+      "Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
+  },
+];
 
 const HomeHero = () => {
   return <div></div>;
